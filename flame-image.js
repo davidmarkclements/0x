b92203f1cd042c730fe8c5b600544df13ed1982f
davidmarkclements <huperekchuno@googlemail.com>
2016-02-26 02:25:14 +0000

    term checks for image preview

--- flame-image.js
+++ flame-image.js
@@ -1,19 +1,11 @@
 var fs = require('fs')
 var spawn = require('child_process').spawn
+var exec = require('child_process').exec
 var which = require('which')
 var debug = require('debug')('0x:flame-image')
 
 module.exports = function (svg, opts, next, done) {
-  //todo work out terminal background colour
-  //todo get height of terminal, make flamegraph 1/3rd of height
-
-  if (process.env.TERM_PROGRAM !== 'iTerm.app') {
-    debug('not iTerm')
-    next()
-    done()
-    return
-  }
-  //TODO check iterm version somehow (or just check for ansi image support somehow)
+  debug('checking for convert binary')
   which('convert', function (err, path) {
     if (err) { 
       debug('imagemagick is not installed')
@@ -21,6 +13,7 @@ module.exports = function (svg, opts, next, done) {
       done()
       return 
     }
+
     var dir = opts.dir || '.'
     debug('writing svg')
     fs.writeFileSync(dir + '/flamegraph.svg', svg)
@@ -31,8 +24,52 @@ module.exports = function (svg, opts, next, done) {
     spawn('convert', args)
       .on('close', function () {
         next()
-        spawn('bash', [__dirname + '/imgcat', dir + '/flamegraph-small.png'], {stdio: 'inherit'})
-          .on('close', done)
+
+
+        debug('checking for iTerm')
+        if (process.env.TERM_PROGRAM !== 'iTerm.app') {
+          debug('not iTerm')
+          console.log(dir + '/flamegraph.svg')
+          next()
+          done()
+          return
+        }
+
+        exec('defaults read com.googlecode.iterm2', function (err, stdout, stderr) {
+          if (err || stderr) {
+            debug('Unable to determine iTerm version')
+            console.log(dir + '/flamegraph.svg')
+            next()
+            done()
+            return
+          }
+          var match = (stdout + '').match(/"iTerm Version" = "(.+)"/)
+          if (!match) {
+            debug('Unable to determine iTerm version')
+            console.log(dir + '/flamegraph.svg')
+            next()
+            done()
+            return
+          }
+          var version = parseFloat(match[1])
+          if (isNaN(version)) {
+            debug('Unable to determine iTerm version')
+            console.log(dir + '/flamegraph.svg')
+            next()
+            done()
+            return
+          }
+          if (version < 2.9) {
+            debug('iTerm version too low to display image - ' + match[1])
+            console.log(dir + '/flamegraph.svg')
+            next()
+            done()
+            return
+          }
+
+          spawn('bash', [__dirname + '/imgcat', dir + '/flamegraph-small.png'], {stdio: 'inherit'})
+            .on('close', done)
+        })
       })
   })
 }
