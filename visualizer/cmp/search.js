69d6faf6dcbc49e0502a96c6e66df5acecc05864
Renée Kooi <renee@kooi.me>
2018-07-04 11:47:39 +0200

    Style fixes
    - Fixes the height of the graph in FF
      For some reason `height: calc(100% - 66px)` does something totally
      different than `height: 100%`, but with `100vh` the behaviour is the
      same either way. This seems like a FF CSS bug maybe…
    - Use floating + margins instead of absolute positions for top bar
      controls
      Default button and input sizes are different across browsers and OSes,
      by using floats + margins the positioning adapts to sizes instead of
      having things drawn on top of each other.

--- visualizer/cmp/search.js
+++ visualizer/cmp/search.js
@@ -4,7 +4,7 @@ const debounce = require('debounce')
 
 module.exports = (render) => (action) => {
   const search = render`
-    <input type="search" placeholder="search functions" class='absolute right-0 top-0 mt2 f5 mr1'>
+    <input type="search" placeholder="search functions" class='fr f5 mr1'>
   `
   search.addEventListener('keydown', debounce(({ target }) => action({type: 'key', value: target.value}), 150))
 
