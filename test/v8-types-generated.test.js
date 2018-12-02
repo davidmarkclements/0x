0cd25f69a2d57d0b9b0e2e46159f04f820a85518
AlanSl <alan.slater@nearform.com>
2018-12-02 11:36:35 +0000

    Restructure test to use async/await

--- test/v8-types-generated.test.js
+++ test/v8-types-generated.test.js
@@ -86,94 +86,78 @@ test('Ensure eval sanitising works as expected before using fixture', function (
   t.end()
 })
 
-test('Generate profile and test its output', function (outerTest) {
-  outerTest.plan(3)
-
+test('Generate profile and test its output', async function (t) {
   const readFile = promisify(fs.readFile)
   let dir
-
-  zeroX({
-    argv: [ resolve(__dirname, './fixture/do-eval.js') ],
-    workingDir: resolve('./')
-  }).catch(onError)
-    .then(testZeroXOutput, onError)
-    .then(readFile, onError)
-    .then(testJsonContents, onError)
-    .then(cleanup, onError)
-    .then(outerTest.end, onError)
-
-  function onError (err) {
+  const cleanup = function () {
+    if (dir) {
+      t.ok(fs.existsSync(dir))
+      rimraf.sync(dir)
+      t.notOk(fs.existsSync(dir))
+    }
+    t.end()
+  }
+  const onError = function (err) {
     cleanup()
     throw err
   }
 
-  function testZeroXOutput (htmlLink) {
-    const htmlFile = htmlLink.replace(/^file:\/\//, '')
+  const htmlLink = await zeroX({
+    argv: [ resolve(__dirname, './fixture/do-eval.js') ],
+    workingDir: resolve('./')
+  }).catch(onError)
 
-    outerTest.test('Test 0x output exists as expected', function (t) {
-      t.ok(htmlFile.includes('flamegraph.html'))
-      t.ok(fs.existsSync(htmlFile))
-      t.ok(fs.statSync(htmlFile).size > 10000)
-      t.end()
-    })
+  const htmlFile = htmlLink.replace(/^file:\/\//, '')
 
-    dir = htmlFile.replace('flamegraph.html', '')
-    const jsonFile = fs.readdirSync(dir).find(name => name.match(/\.json$/))
-    return path.resolve(dir, jsonFile)
-  }
+  // Test 0x output exists as expected
+  t.ok(htmlFile.includes('flamegraph.html'))
+  t.ok(fs.existsSync(htmlFile))
+  t.ok(fs.statSync(htmlFile).size > 10000)
 
-  function testJsonContents (content) {
-    const jsonArray = JSON.parse(content).code
+  dir = htmlFile.replace('flamegraph.html', '')
+  const jsonFile = fs.readdirSync(dir).find(name => name.match(/\.json$/))
 
-    const app = jsonArray.find(item => item.name.match(/^appOuterFunc /))
-    const appUnicode = jsonArray.find(item => item.name.match(/^doFunc.+μИاκهよΞ[/\\]unicode-in-path\.js/))
-    const appLongMethod = jsonArray.find(item => item.type === 'JS' && item.name.match(/^method: \\μИاκهよΞ\\ \[CODE:RegExp] \/ native \/ \[SHARED_LIB]/))
+  const content = await readFile(path.resolve(dir, jsonFile)).catch(onError)
 
-    const deps = jsonArray.find(item => item.name.match(/node_modules[/\\]debounce/))
+  const jsonArray = JSON.parse(content).code
 
-    // We get into multi-level escape character hell if we try to escape then match against the original strings
-    // Duplicate stubs long enough to check it's a) correctly classified, and b) unicode etc isn't mangled
-    const matchRegexPaths = /^\/D:\u005cDocuments and Settings\u005cАлександра ǂǐ-sì\u005cinternal\u005capp native internal\u005cnode_modules\u005csome-module\u005cesm.mjs:1:1/
-    const regexPaths = jsonArray.find(item => item.name.match(matchRegexPaths))
-    const matchRegexNonPath = /^\/\[\/\u005c] \.js native \.mjs \u005c \/ :\u005c \/ \u005c \u005c\u005cserver \(\u005cusers\u005cu2fan\u005cnode_modules\u005c\|\/node_modules\/\) \[eval].js:1:2/
-    const regexNonPath = jsonArray.find(item => item.name.match(matchRegexNonPath))
+  const app = jsonArray.find(item => item.name.match(/^appOuterFunc /))
+  const appUnicode = jsonArray.find(item => item.name.match(/^doFunc.+μИاκهよΞ[/\\]unicode-in-path\.js/))
+  const appLongMethod = jsonArray.find(item => item.type === 'JS' && item.name.match(/^method: \\μИاκهよΞ\\ \[CODE:RegExp] \/ native \/ \[SHARED_LIB]/))
 
-    const evalFunc = jsonArray.find(item => item.type === 'JS' && item.name.match(/^evalInnerFunc /))
+  const deps = jsonArray.find(item => item.name.match(/node_modules[/\\]debounce/))
 
-    outerTest.test('Test 0x json contents are logged and classified as expected', function (t) {
-      t.ok(app)
-      t.equal(getType(app), 'app')
+  // We get into multi-level escape character hell if we try to escape then match against the original strings
+  // Duplicate stubs long enough to check it's a) correctly classified, and b) unicode etc isn't mangled
+  const matchRegexPaths = /^\/D:\u005cDocuments and Settings\u005cАлександра ǂǐ-sì\u005cinternal\u005capp native internal\u005cnode_modules\u005csome-module\u005cesm.mjs:1:1/
+  const regexPaths = jsonArray.find(item => item.name.match(matchRegexPaths))
+  const matchRegexNonPath = /^\/\[\/\u005c] \.js native \.mjs \u005c \/ :\u005c \/ \u005c \u005c\u005cserver \(\u005cusers\u005cu2fan\u005cnode_modules\u005c\|\/node_modules\/\) \[eval].js:1:2/
+  const regexNonPath = jsonArray.find(item => item.name.match(matchRegexNonPath))
 
-      t.ok(appUnicode)
-      t.equal(getType(appUnicode), 'app')
+  const evalFunc = jsonArray.find(item => item.type === 'JS' && item.name.match(/^evalInnerFunc /))
 
-      t.ok(appLongMethod)
-      t.equal(getType(appLongMethod), 'app')
+  // Test 0x json contents are logged and classified as expected
+  t.ok(app)
+  t.equal(getType(app), 'app')
 
-      t.ok(deps)
-      t.equal(getType(deps), 'deps')
+  t.ok(appUnicode)
+  t.equal(getType(appUnicode), 'app')
 
-      t.ok(regexPaths)
-      t.equal(getType(regexPaths), 'regexp')
+  t.ok(appLongMethod)
+  t.equal(getType(appLongMethod), 'app')
 
-      t.ok(regexNonPath)
-      t.equal(getType(regexNonPath), 'regexp')
+  t.ok(deps)
+  t.equal(getType(deps), 'deps')
 
-      t.ok(evalFunc)
-      t.equal(getType(evalFunc), 'native')
-      t.ok(getProcessedName(evalFunc).includes('[eval]'))
+  t.ok(regexPaths)
+  t.equal(getType(regexPaths), 'regexp')
 
-      t.end()
-    })
-  }
+  t.ok(regexNonPath)
+  t.equal(getType(regexNonPath), 'regexp')
 
-  function cleanup () {
-    outerTest.test('Test cleanup works as expected', function (t) {
-      t.ok(fs.existsSync(dir))
-      rimraf.sync(dir)
-      t.notOk(fs.existsSync(dir))
+  t.ok(evalFunc)
+  t.equal(getType(evalFunc), 'native')
+  t.ok(getProcessedName(evalFunc).includes('[eval]'))
 
-      t.end()
-    })
-  }
+  cleanup()
 })
