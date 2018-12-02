b86691df3e141c35893c4a3c1804bcc969c2045c
AlanSl <alan.slater@nearform.com>
2018-12-02 16:56:53 +0000

    Split static tests into two files
    Make sure it reliably doesn't time out

--- test/v8-types-static.test.js
+++ test/v8-types-static.test.js
@@ -32,34 +32,9 @@ const {
   appWindows
 } = require('./util/type-edge-cases.js')
 
-const {
-  getProcessedName,
-  getType
-} = require('./util/classify-frames.js')
+const { getType } = require('./util/classify-frames.js')
 
-test('Test typical examples - backend name transformation', function (t) {
-  t.equal(getProcessedName(init1), init1.name + ' [INIT]')
-  t.equal(getProcessedName(init2), init2.name + ' [INIT]')
-  t.equal(getProcessedName(cpp1), cpp1.name + ' [SHARED_LIB]')
-  t.equal(getProcessedName(cpp2), cpp2.name + ' [SHARED_LIB]')
-  t.equal(getProcessedName(v81), v81.name + ' [CPP]')
-  t.equal(getProcessedName(v82), v82.name + ' [CODE:BuiltIn]')
-  t.equal(getProcessedName(regexp1), regexp1.name + ' [CODE:RegExp]')
-  t.equal(getProcessedName(regexp2), regexp2.name + ' [CODE:RegExp]')
-  t.equal(getProcessedName(native1), '~' + native1.name)
-  t.equal(getProcessedName(native2), '*(anonymous) [eval]:486:24')
-  t.equal(getProcessedName(core1), '*' + core1.name)
-  t.equal(getProcessedName(core2), '~' + core2.name)
-  t.equal(getProcessedName(deps1), '*' + deps1.name)
-  t.equal(getProcessedName(deps2), '~' + deps2.name)
-  t.equal(getProcessedName(app1), '~' + app1.name)
-  t.equal(getProcessedName(app2), '*' + app2.name)
-  t.equal(getProcessedName(inlinable, true), '~' + inlinable.name + ' [INLINABLE]')
-
-  t.end()
-})
-
-test('Test typical examples - frontend name parsing', function (t) {
+test('Test typical examples - frontend types from names', function (t) {
   t.equal(getType(init1), 'init')
   t.equal(getType(init2), 'init')
   t.equal(getType(cpp1), 'cpp')
