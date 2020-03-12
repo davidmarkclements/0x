d2f049b9377be5b2ddc54e610a2a8545edcfb41f
Dylan <dylan_coakley@yahoo.co.uk>
2020-03-12 17:38:29 +0000

    Update to latest version of transpile check fn

--- index.js
+++ index.js
@@ -116,19 +116,16 @@ function checkForTranspiledCode (filename) {
   let isTranspiled = false
 
   // Check for a source map
-  isTranspiled = readFile.includes('//# sourceMappingURL=')
-
-  while ((matchedObj = regex.exec(readFile)) !== null) {
-    // Avoid infinite loops with zero-width matches
-    if (matchedObj.index === regex.lastIndex) {
-      regex.lastIndex++
-    }
+  if (readFile.includes('//# sourceMappingURL=')) {
+    isTranspiled = true
+  } else {
+    isTranspiled = true
     // Loop through results and check length of fn name
-    matchedObj.forEach((match, groupIndex) => {
-      if (groupIndex !== 0 && match.length < 3) {
-        isTranspiled = true
+    while ((matchedObj = regex.exec(readFile)) !== null) {
+      if (matchedObj[1].length > 3) {
+        isTranspiled = false
       }
-    })
+    }
   }
   return isTranspiled
 }
