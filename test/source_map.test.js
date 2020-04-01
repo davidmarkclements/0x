b14774371648eda579fdd8d6ee3054fbe2a1aba7
Dylan <dylan_coakley@yahoo.co.uk>
2020-04-01 10:25:40 +0100

    Update source map test

--- test/source_map.test.js
+++ test/source_map.test.js
@@ -1,4 +1,25 @@
 const { test } = require('tap')
+const zeroX = require('../')
+const { promisify } = require('util')
+const fs = require('fs')
+const { resolve } = require('path')
+
+
+test('Generate profile and test its output', async function (t) {
+  const readFile = promisify(fs.readFile)
+  let dir
+  const cleanup = function () {
+    if (dir) {
+      t.ok(fs.existsSync(dir))
+      rimraf.sync(dir)
+      t.notOk(fs.existsSync(dir))
+    }
+    t.end()
+  }
+  const onError = function (err) {
+    cleanup()
+    throw err
+  }
 
  const htmlLink = await zeroX({
     argv: [ resolve(__dirname, './fixture/sourcemap.min.js') ],
@@ -6,3 +27,6 @@ const { test } = require('tap')
     collectOnly: true,
   	treeDebug: true
   }).catch(onError)
+
+cleanup()
+})
