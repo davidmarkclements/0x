b1f88fc52f2126c5dc02ea1e4d25100ff8b184fe
Osmond van Hemert <osmond@vanhemert.nu>
2020-09-24 13:25:42 +0200

    update path

--- test/basic.test.js
+++ test/basic.test.js
@@ -11,7 +11,7 @@ test('accepts different workingDir', async t => {
   const zeroEks = require('../')
   const opts = {
     argv: [ resolve(__dirname, './fixture/do-eval.js') ],
-    workingDir: resolve('../test.0x')
+    workingDir: resolve('./')
   }
   const file = zeroEks(opts)
   t.resolves(file)
