6167e01fecc9099900bd469b5c33428957641e15
Osmond van Hemert <osmond@vanhemert.nu>
2020-09-24 10:42:58 +0200

    fix: error msg on workingdir

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,7 +1,19 @@
+const path = require('path')
 const { test } = require('tap')
 
 test('module loads', function (t) {
   require('../')
   t.pass('0x loaded')
+  t.end()
+})
+
+test('accepts different workingDir', async t => {
+  const zeroEks = require('../')
+  const opts = {
+    argv: [path.join(__dirname, 'fixture', 'do-eval.js')],
+    workingDir: path.join(__dirname, '..', 'test.0x')
+  }
+  const file = zeroEks(opts)
+  t.resolves(file)
   t.end()
 })
