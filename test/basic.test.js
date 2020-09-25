06587b017b882ef4366286d5ca418d05329b06d3
Osmond van Hemert <osmond@vanhemert.nu>
2020-09-25 10:41:31 +0200

    fix: error msg on workingdir (#232)
    * fix: error msg on workingdir
    
    * make it work on ci
    
    * update path

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,7 +1,19 @@
+const { resolve } = require('path')
 const { test } = require('tap')
 
 test('module loads', function (t) {
   require('../')
   t.pass('0x loaded')
+  t.end()
+})
+
+test('accepts different workingDir', async t => {
+  const zeroEks = require('../')
+  const opts = {
+    argv: [ resolve(__dirname, './fixture/do-eval.js') ],
+    workingDir: resolve('./')
+  }
+  const file = zeroEks(opts)
+  t.resolves(file)
   t.end()
 })
