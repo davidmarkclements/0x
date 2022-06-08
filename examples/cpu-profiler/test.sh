93b1d37ffa31d7e68282b6f46165da8c05bfd7f8
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2022-06-08 21:02:40 +0800

    Support `.cpuprofile` of `node --cpu-prof`

--- examples/cpu-profiler/test.sh
+++ examples/cpu-profiler/test.sh
@@ -7,7 +7,8 @@ sleep 1
 
 curl -v http://localhost:3000/start-profiling
 
-node node_modules/.bin/autocannon -l -R 200000 -c 2000 -d 20 http://localhost:3000
+node --cpu-prof --cpu-prof-name=samples-cpu-prof.cpuprofile \
+  node_modules/.bin/autocannon -l -R 200000 -c 2000 -d 20 http://localhost:3000
 
 curl -v http://localhost:3000/stop-profiling
 
@@ -15,3 +16,4 @@ sleep 3
 kill -KILL $first_pid
 
 node node_modules/.bin/0x --visualize-cpu-profile samples.cpuprofile
+node node_modules/.bin/0x --name=flamegraph-autocannon --visualize-cpu-profile samples-cpu-prof.cpuprofile
